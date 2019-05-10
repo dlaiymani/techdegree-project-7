a99a63344c91c753ea19aac630ef44787dca0fe7
David laiymani <david.laiymani@gmail.com>
2019-05-10 16:03:58 +0200

    Initial commit

---
+++ MovieNight/MovieNight/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  MovieNight
+//
+//  Created by davidlaiymani on 10/05/2019.
+//  Copyright © 2019 davidlaiymani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+        backgroundImage.image = UIImage(named: "background")
+        backgroundImage.contentMode = UIView.ContentMode.scaleAspectFill
+        self.view.insertSubview(backgroundImage, at: 0)
+
+
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        navigationController?.navigationBar.barStyle = .black
+    }
+
+}
+
