a3298049cbdea571a84073de172a63cde27b59c1
David laiymani <david.laiymani@gmail.com>
2019-05-12 11:49:26 +0200

    Movie Detail implemented

---
+++ MovieNight/MovieNight/Controllers/DetailMovieController.swift
@@ -0,0 +1,31 @@
+//
+//  DetailMovieController.swift
+//  MovieNight
+//
+//  Created by davidlaiymani on 12/05/2019.
+//  Copyright © 2019 davidlaiymani. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class DetailMovieController: UIViewController {
+    
+    @IBOutlet weak var posterImage: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    var movie: Movie?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let movie = movie {
+            posterImage.image = movie.poster
+            titleLabel.text = movie.name
+            overviewLabel.text = movie.overview
+        }
+    }
+}
--- MovieNight/MovieNight/Controllers/MovieListController.swift
+++ MovieNight/MovieNight/Controllers/MovieListController.swift
@@ -83,14 +83,21 @@ class MovieListController: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        
+        if segue.identifier == "DetailMovieSegue" {
+            let indexPath = self.tableView.indexPathForSelectedRow!
+            let movie = dataSource.object(at: indexPath)
+            let detailMovieController = segue.destination as! DetailMovieController
+            detailMovieController.movie = movie
+            
+        }
+        
     }
-    */
+    
 
 }
